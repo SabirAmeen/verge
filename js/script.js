04c050605f29c2890a2260874eb6f37bbd9c07fb
Sabir <sabir@qburst.com>
2016-08-24 18:32:43 +0530

    Completed Trending news fixed isuues

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $('.menu').click(function(){
 	$('.nav-bar').css("border-right","2px solid #fa4b2a");
 	$('.transparent-nav').css("width","100%");
 	$('.transparent-nav').css("left","240px");
-
 });
 $('.transparent-nav').click(function(){
 	$('.nav-bar').css("width","0px");
@@ -23,4 +22,17 @@ $('.transparent-art').click(function(){
 	$('.updated-articles').css("border-left","0px");
 	$('.transparent-art').css("width","0px");
 	$('.transparent-art').css("right","0px");
+});
+$('#trending-dropdown-btn').click(function(){
+	$('#trending-dropdown').slideToggle("fast");
+	if($('#dropdown-arrow').text()==="\u25BC")
+	{	$('#dropdown-arrow').text("\u25B2");
+		$('#trending-dropdown-btn').css("background-color","#fa4b2a");
+		$('#dropdown-arrow').css("color","white");
+	}
+	else
+	{	$('#dropdown-arrow').text("\u25BC");
+		$('#trending-dropdown-btn').css("background-color","#f0f0f0");
+		$('#dropdown-arrow').css("color","#fa4b2a");
+	}
 });
